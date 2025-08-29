70e618c586176667b5610a2eb210718dce483da2
angela <angela.delpozo@digital.gob.es>
2025-08-29 12:12:14 +0200

    segundo commit hecho desde VSCode

--- angela_prueba.py
+++ angela_prueba.py
@@ -1,5 +1,12 @@
 
-import os
+import os, sys
 
 
 print("hola mundo")
+
+input_file = sys.argv[1]
+
+if os.path.exists(input_file):
+    print('Existe el fichero: %s' % (input_file))
+else:
+    sys.exit(2)
